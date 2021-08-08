e8cbae97ee3907f37fe8e0b26f1ffb8386507e4d
joelgroomer <joelgroomer@users.noreply.github.com>
2021-08-07 17:34:26 -0700

    Added defaults for portfolio images; fixed default topic bug for blogs

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,4 +11,11 @@ class Portfolio < ApplicationRecord
   ## Method 2 - `scope` which returns a lambda of the method instead
   ## call in controller using `Portfolio.ruby_on_rails_portfolio_items`
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
